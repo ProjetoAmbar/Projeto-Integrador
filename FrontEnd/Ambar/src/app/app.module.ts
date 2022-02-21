0f309bf272e2b26d25964828559f4ae9f3884ca5
unknown <you@example.com>
2022-02-21 16:13:30 -0300

    Esqueleto do FrontEnd

---
+++ FrontEnd/Ambar/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EntrarComponent,
+    MenuComponent,
+    RodapeComponent,
+    CadastrarComponent,
+    SobreNosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ FrontEnd/Ambar/src/app/sobre-nos/sobre-nos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre-nos',
+  templateUrl: './sobre-nos.component.html',
+  styleUrls: ['./sobre-nos.component.css']
+})
+export class SobreNosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
