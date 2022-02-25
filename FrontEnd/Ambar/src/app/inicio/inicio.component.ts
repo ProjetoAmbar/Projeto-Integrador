076971f83992472c0144a89900f1d5e563419220
unknown <you@example.com>
2022-02-25 16:02:07 -0300

    @Gabriel.P.F  Inicio Vr:0.5

--- FrontEnd/Ambar/src/app/inicio/inicio.component.ts
+++ FrontEnd/Ambar/src/app/inicio/inicio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    if(environment.token == ''){
+      /*alert('Sua seção expirou, faça o login novamente')*/
+      this.router.navigate(['/entrar'])}
   }
 
 }
